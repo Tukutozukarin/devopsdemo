bfa772d4167afa7a88c77de908c3b9f1ddfc4b07
Tukutozukarin <benle98@live.com>
2019-09-02 11:33:27 +0200

    all files

--- src/main/java/com/demoproject/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/demoproject/devopsdemo/DevopsdemoApplication.java
@@ -2,13 +2,14 @@ package com.demoproject.devopsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DevopsdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsdemoApplication.class, args);
-		SpringApplication.run(Lab3Application.class, args);
 	}
 
 	@RestController

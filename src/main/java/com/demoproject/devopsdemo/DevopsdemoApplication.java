664b367eab6ad4bf21e08c44152e9e19be500c51
Tukutozukarin <benle98@live.com>
2019-10-21 10:52:35 +0200

    irst commit

--- src/main/java/com/demoproject/devopsdemo/DevopsdemoApplication.java
+++ src/main/java/com/demoproject/devopsdemo/DevopsdemoApplication.java
@@ -1,9 +1,12 @@
 package com.demoproject.devopsdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class DevopsdemoApplication {
@@ -21,5 +24,16 @@ public class DevopsdemoApplication {
 		}
 
 	}
+
+	public class LogzioLogbackExample {
+		public void main(String[] args) {
+			Logger logger = LoggerFactory.getLogger(LogzioLogbackExample.class);
+
+			logger.info("Testing logz.io!");
+			logger.warn("Winter is coming");
+		}
+	}
+
+
 }
 
